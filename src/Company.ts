b142dccfe6f772a10b09bae2ab07bc60e647ded0
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 22:39:43 +0200

    Company.ts implemented

---
+++ src/Company.ts
@@ -0,0 +1,21 @@
+import faker from 'faker'
+
+export class Company {
+    companyName: string;
+    catchPhrase: string;
+    location: {
+        lat: number,
+        lng: number
+    }
+
+    constructor() {
+        this.companyName = faker.company.companyName()
+        this.catchPhrase = faker.company.catchPhrase()
+
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+
+}
