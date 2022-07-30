25b606aba150eb50aeb6a997180ae03b0bef01ba
Renan Bezerra <renanbezerraenes@icloud.com>
2022-07-30 22:27:43 +0200

    Using faker library to generate random latitude, longitude and name

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,18 @@
+import faker from 'faker'
+import { forEachLeadingCommentRange } from 'typescript';
+
 class User {
-    
+    name: string;
+    location: {
+        lat: number,
+        lng: number
+    }
+
+    constructor() {
+        this.name = faker.name.firstName()
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
 }
